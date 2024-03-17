603df5bd5d1bb1da38431e2fc635d3938b71e6a1
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-03-17 13:11:25 +0530

    URL catch

--- ixten/src/App.js
+++ ixten/src/App.js
@@ -1,30 +1,41 @@
-import logo from './logo.svg';
-import { StatsRing } from './components/StatsRing';
-import { RingProgress } from '@mantine/core';
-import './App.css';
+/*global chrome*/
+import logo from "./logo.svg";
+import { StatsRing } from "./components/StatsRing";
+import { RingProgress } from "@mantine/core";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data,setData]=useState("");
   return (
     <div className="text-center	 bg-[#282c34]">
       <div className="min-h-[100vh] flex flex-col justify-center items-center text-white gap-16">
-        
-        <div className='flex flex-row gap-5 justify-center items-center p-3  border-4 border-white rounded-2xl'>
-        <RingProgress
-      size={180}
-      thickness={21}
-      roundCaps
-      sections={[
-        { value: 40, color: 'cyan' },
-      ]}
-    >Leaffff</RingProgress>
-             <h1 className='text-5xl font-bold'>40%</h1>
-
+        <div className="flex flex-row gap-5 justify-center items-center p-3  border-4 border-white rounded-2xl">
+          <RingProgress
+            size={180}
+            thickness={21}
+            roundCaps
+            sections={[{ value: 40, color: "cyan" }]}
+         / >
+          <h1 className="text-5xl font-bold">40%</h1>
         </div>
-        
-        <p className='text-2xl font-semibold'>
-        Anti Sus Browsing <br/><br/>
-       <span className='text-lg'> A chrome extension to detect suspicious activity in online browsing</span>
+
+        <p className="text-2xl font-semibold">
+          Anti Sus Browsing <br />
+          <br />
+          <span className="text-lg">
+            {" "}
+            A chrome extension to detect suspicious activity in online browsing
+          </span>
         </p>
+        {chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function(tabs) {
+    var tabURL = tabs[0].url;
+    console.log(tabURL);
+    setData(tabURL)
+})} {data}
       </div>
     </div>
   );
