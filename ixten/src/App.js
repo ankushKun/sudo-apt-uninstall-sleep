d82756593f27489217c254ce26aa5227fd28e8f2
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-03-17 12:18:49 +0530

    changing UI and css

--- ixten/src/App.js
+++ ixten/src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <StatsRing/> */}
-        <div className='ring'>
+    <div className="text-center	 bg-[#282c34]">
+      <div className="min-h-[100vh] flex flex-col justify-center items-center text-white gap-16">
+        
+        <div className='flex flex-row gap-5 justify-center items-center p-3  border-4 border-white rounded-2xl'>
         <RingProgress
-      size={220}
+      size={180}
       thickness={21}
       roundCaps
       sections={[
         { value: 40, color: 'cyan' },
       ]}
     >Leaffff</RingProgress>
+             <h1 className='text-5xl font-bold'>40%</h1>
+
         </div>
         
-         <h1 className='val'>40%</h1>
-        <p>
-        Anti Sus Browsing <br/>
-        A chrome extension to detect sus activity in online browsing
+        <p className='text-2xl font-semibold'>
+        Anti Sus Browsing <br/><br/>
+       <span className='text-lg'> A chrome extension to detect suspicious activity in online browsing</span>
         </p>
       </div>
     </div>
